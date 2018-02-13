25c3a032f362bd0057bad8e063354e1ed59130d5
wartgowj <wartgowj@gmail.com>
2018-02-12 19:53:29 -0800

    basic routing done

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,10 @@
+
+var friendData = require("../data/friends");
+
+module.exports = function(app) {
+
+app.get("/api/friends", function(req, res){
+    res.json(friendData);
+});
+
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log("Server running on port: " + PORT);
+});
