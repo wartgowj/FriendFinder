f37de965fb18763dc4676bb9fb0310aca448aea2
wartgowj <wartgowj@gmail.com>
2018-02-16 22:04:02 -0800

    more code done

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ var friendsArray = [
     name: "Jaime",
     photo: "https://github.com/wartgowj/responsive-portfolio/blob/master/assets/images/jaime.jpg",
     scores: [
-                5,
-                1,
-                4,
-                4,
-                5,
-                1,
-                2,
-                5,
-                4,
-                1
+                "5",
+                "1",
+                "4",
+                "4",
+                "5",
+                "1",
+                "2",
+                "5",
+                "4",
+                "1"
             ]
 }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,10 @@ app.get("/api/friends", function(req, res){
 
 app.post("/api/friends", function(req, res){
     friendData.push(req.body);
+
+    // console.log(friendData[0].scores);
+    // console.log(friendData[1].scores);
+
 })
 
 };
